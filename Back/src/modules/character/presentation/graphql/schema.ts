50fd0154de07926c39bfed541b3f7def8f514855
Juan Troconis <jatroconis4@gmail.com>
2025-09-01 05:50:19 -0500

    [Fix] Add schema Character from error Query.characters defined in resolvers, but not in schema

--- Back/src/modules/character/presentation/graphql/schema.ts
+++ Back/src/modules/character/presentation/graphql/schema.ts
@@ -1,7 +1,27 @@
 import { gql } from 'graphql-tag';
 
 export const typeDefs = gql`
+  type Character {
+    id: Int!
+    name: String!
+    status: String!
+    species: String!
+    gender: String!
+    origin: String!
+    image: String
+    created: String
+  }
+
+  input CharacterFilter {
+    name: String
+    status: String
+    species: String
+    gender: String
+    origin: String
+  }
+
   type Query {
     ping: String!
+    characters(filter: CharacterFilter): [Character!]!
   }
 `;
